18e1b8085b911fff13e4e34891b4254228295354
Marwin Carmo <marwinbr@gmail.com>
2022-02-02 15:23:47 -0300

    add calendar event

---
+++ assets/calendar.R
@@ -0,0 +1,8 @@
+mp_calendar <- data.frame(
+    title = "Measurement in the Human Sciences w/ Dr. Derek Briggs",
+    body = "EST timezone",
+    start = format(Sys.Date(), "%Y-%m-25 13:00:00"),
+    end = format(Sys.Date(), "%Y-%m-25 14:00:00"),
+    category = "time"
+)
+
