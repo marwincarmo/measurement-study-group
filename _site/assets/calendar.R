7e29c97d2f2d13775ee39895113d420f8ef5da10
Marwin Carmo <marwinbr@gmail.com>
2022-05-20 14:25:46 -0300

    update calendar

--- _site/assets/calendar.R
+++ _site/assets/calendar.R
@@ -11,25 +11,25 @@ titles <- c("Measurement in the Human Sciences w/ Dr. Derek Briggs", "Measuring 
             "Measuring Psychology #12 - Measurement Without Theory",
             "Measuring Psychology #13 - Dr. Derek Briggs pt. 2")
 
-start_time <- c(format(Sys.Date(), "%Y-02-25 13:00:00"), format(Sys.Date(), "%Y-03-11 13:00:00"),
-                format(Sys.Date(), "%Y-03-25 13:00:00"), format(Sys.Date(), "%Y-04-08 13:00:00"),
-                format(Sys.Date(), "%Y-04-22 13:00:00"), format(Sys.Date(), "%Y-05-06 13:00:00"),
-                format(Sys.Date(), "%Y-05-20 13:00:00"), format(Sys.Date(), "%Y-06-03 13:00:00"),
-                format(Sys.Date(), "%Y-06-17 13:00:00"), format(Sys.Date(), "%Y-07-01 13:00:00"),
-                format(Sys.Date(), "%Y-07-15 13:00:00"), format(Sys.Date(), "%Y-07-29 13:00:00"),
-                format(Sys.Date(), "%Y-08-12 13:00:00"))
+start_time <- c(format(Sys.Date(), "%Y-02-25 18:00:00"), format(Sys.Date(), "%Y-03-11 18:00:00"),
+                format(Sys.Date(), "%Y-03-25 18:00:00"), format(Sys.Date(), "%Y-04-08 18:00:00"),
+                format(Sys.Date(), "%Y-04-22 18:00:00"), format(Sys.Date(), "%Y-05-06 18:00:00"),
+                format(Sys.Date(), "%Y-05-20 18:00:00"), format(Sys.Date(), "%Y-06-03 18:00:00"),
+                format(Sys.Date(), "%Y-06-17 18:00:00"), format(Sys.Date(), "%Y-07-01 18:00:00"),
+                format(Sys.Date(), "%Y-07-15 18:00:00"), format(Sys.Date(), "%Y-07-29 18:00:00"),
+                format(Sys.Date(), "%Y-08-12 18:00:00"))
 
-end_time <- c(format(Sys.Date(), "%Y-02-25 14:00:00"), format(Sys.Date(), "%Y-03-11 14:00:00"),
-              format(Sys.Date(), "%Y-03-25 14:00:00"), format(Sys.Date(), "%Y-04-08 14:00:00"),
-              format(Sys.Date(), "%Y-04-22 14:00:00"), format(Sys.Date(), "%Y-05-06 14:00:00"),
-              format(Sys.Date(), "%Y-05-20 14:00:00"), format(Sys.Date(), "%Y-06-03 14:00:00"),
-              format(Sys.Date(), "%Y-06-17 14:00:00"), format(Sys.Date(), "%Y-07-01 14:00:00"),
-              format(Sys.Date(), "%Y-07-15 14:00:00"), format(Sys.Date(), "%Y-07-29 14:00:00"),
-              format(Sys.Date(), "%Y-08-12 14:00:00"))
+end_time <- c(format(Sys.Date(), "%Y-02-25 19:00:00"), format(Sys.Date(), "%Y-03-11 19:00:00"),
+              format(Sys.Date(), "%Y-03-25 19:00:00"), format(Sys.Date(), "%Y-04-08 19:00:00"),
+              format(Sys.Date(), "%Y-04-22 19:00:00"), format(Sys.Date(), "%Y-05-06 19:00:00"),
+              format(Sys.Date(), "%Y-05-20 19:00:00"), format(Sys.Date(), "%Y-06-03 19:00:00"),
+              format(Sys.Date(), "%Y-06-17 19:00:00"), format(Sys.Date(), "%Y-07-01 19:00:00"),
+              format(Sys.Date(), "%Y-07-15 19:00:00"), format(Sys.Date(), "%Y-07-29 19:00:00"),
+              format(Sys.Date(), "%Y-08-12 19:00:00"))
 
 mp_calendar <- data.frame(
     title = titles,
-    body = "EST timezone",
+    body = "GMT +0 timezone",
     start = start_time,
     end = end_time,
     category = "time",
